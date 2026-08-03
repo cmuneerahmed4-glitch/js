418b4eac20932e32ea36ef842758d63f7071ed6f
cmuneerahmed4-glitch <cmuneerahmed4@gmail.com>
2026-08-02 13:28:55 -1200

    task3

---
+++ task3.js
@@ -0,0 +1,10 @@
+let i = 1;
+while (i <= 15) {
+    if (i % 3 === 0) {
+        console.log (i +  "Multiple of 3");
+    } else {
+        console.log(i +"Not Multiple");
+    } i++;
+}
+//har number check hoga.
+//agar sy divide ho jaye to multiole print hoga.
