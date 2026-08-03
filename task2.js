a2128317bc017e05bb727439518725fe0ec850c8
cmuneerahmed4-glitch <cmuneerahmed4@gmail.com>
2026-08-02 13:19:47 -1200

    task 2

--- task2.js
+++ task2.js
@@ -1,6 +1,15 @@
 let i = -5;
 while (i <= 5){
     if (i >0){
-        console.log{i+ "= Po"}
+        console.log{i+ "= Positive"};
+
     }
+    else if (i < 0){
+        console.log(i + "=Negative");
+    }else {console.log(i + "=Zero");
+
+    }i++;
 }
+//roman urdu 
+//har number ko checkkaro.
+// positive . negative ya zero print karo.
