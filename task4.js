4929545217248c5aad5761d0c77856af1a8978d7
cmuneerahmed4-glitch <cmuneerahmed4@gmail.com>
2026-08-02 13:35:32 -1200

    task4

---
+++ task4.js
@@ -0,0 +1,11 @@
+let i = 1;
+while (i <= 10){
+    if (i > 5){
+        console.log(i +"Greater");
+    } else {
+        console.log(i +"Smaller");
+    }
+    i++;
+}
+//5 sy bara number Greater.
+//warna Smaller.
