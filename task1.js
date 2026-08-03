ad3cc3d77f0f5ef555d8aefb1cc33de185377d5f
cmuneerahmed4-glitch <cmuneerahmed4@gmail.com>
2026-08-02 13:10:30 -1200

    taskjs

---
+++ task1.js
@@ -0,0 +1,13 @@
+let i=10;
+while (i <=10){
+    if (i % 2 === 0){
+        console.log(i+ "=Even");
+    } else {
+        console.log (i + "=Odd");
+    }
+    i++;
+}
+//roman urdu;
+//loop 1 sy 10 tak chalega.
+//har number remainder 0 aya to Even.
+//warna odd print hoga.
---
+++ task2.js
@@ -0,0 +1,6 @@
+let i = -5;
+while (i <= 5){
+    if (i >0){
+        console.log{i+ "= Po"}
+    }
+}
