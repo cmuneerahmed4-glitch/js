7293752ec347dbc4bca49a9f1dcee466121b74a1
cmuneerahmed4-glitch <cmuneerahmed4@gmail.com>
2026-08-02 13:49:50 -1200

    task5

---
+++ task5.js
@@ -0,0 +1,14 @@
+let i = 1;
+
+while (i <= 20){
+
+    if (i % 5 === 0){
+  console.log(i + "Divisible" );
+
+} else {
+  
+console.log (i);
+} i++;
+}
+// roman urdu.
+//Sirf 5 ke multiples ko divisible likho.
