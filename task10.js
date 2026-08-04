60fefe914ceb788974a1b4696579374171a24263
cmuneerahmed4-glitch <cmuneerahmed4@gmail.com>
2026-08-03 14:37:03 -1200

    tasks

---
+++ task10.js
@@ -0,0 +1,13 @@
+let i = 10 ;
+while (i >= 1){
+    if (i % 2 === 0 ){
+        console.log(i + "Even");
+    } else {
+        console.log(i + "Odd");
+    } i__
+
+    }
+    console.log ("Blast Off!");
+    //roman urdu.
+    // reverse couting hogi.
+    //har number ka Even/Odd bhi check hoga.
---
+++ task8.js
@@ -0,0 +1,12 @@
+ let marks = 40 ;
+        while (marks <= 100){
+            if (marks >= 50){
+                console.log (marks + "Pass");
+            } else {
+                console.log(marks + "Fail");
+            }
+            marks += 10;
+        }
+        //Roman urdu
+        //40 ya us 100 tak marks check karo.
+        //50 ya us sy zyada pass.
